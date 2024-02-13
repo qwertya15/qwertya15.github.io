5a545eebc6c05489b9af9b278d0b1a64500b0bd8
qwertya15 <48073050+qwertya15@users.noreply.github.com>
2024-02-12 21:53:07 -0500

    update site

--- script.js
+++ script.js
@@ -13,20 +13,31 @@ document.querySelector('.nav-toggle').onclick=()=>nav.classList.toggle('open');
 
 
 // fancy project card tilting setup
+let fancyCards=true;
 function setupCard(c,c1) {
   let hovering=false,deg=0.025;
+  // setup
+  reset();
+  c1.style.transform=`perspective(400px) rotateX(var(--rx)) rotateY(var(--ry))`;
+  c1.style.background=`radial-gradient(at var(--bx) var(--by), var(--t-bg1), #0000) var(--t-bg3)`;
   // calculations
+  function reset() {
+    hovering=false;
+    c1.style.setProperty('--rx','0deg');
+    c1.style.setProperty('--ry','0deg');
+    c1.style.setProperty('--bx','25%');
+    c1.style.setProperty('--by','25%');
+  }
   function tilt(e) {
+    if (!fancyCards) return reset();
     let x=(e.clientX-c.offsetLeft+window.scrollX),
         y=(e.clientY-c.offsetTop+window.scrollY);
     let dx=(c.offsetWidth/2)-x,
         dy=(c.offsetHeight/2)-y;
-    c1.style.transform=`rotateX(${dy*deg}deg) rotateY(${-dx*deg}deg)`;
-    c1.style.background=`radial-gradient(at ${100-((x/c.offsetWidth)*100)}% ${100-((y/c.offsetHeight)*100)}%,var(--t-bg2),#0000) var(--t-bg4)`;
-  }
-  function reset() {
-    c1.style.transform=null;
-    c1.style.background=null;
+    c1.style.setProperty('--rx',`${dy*deg}deg`);
+    c1.style.setProperty('--ry',`${-dx*deg}deg`);
+    c1.style.setProperty('--bx',`${100-((x/c.offsetWidth)*100)}%`);
+    c1.style.setProperty('--by',`${100-((y/c.offsetHeight)*100)}%`);
   }
   // add listeners
   c.addEventListener('pointerover',e=>{
@@ -44,3 +55,5 @@ function setupCard(c,c1) {
   },{passive:true});
 }
 document.querySelectorAll('.cards .card').forEach(e=>setupCard(e,e.querySelector('.card-details')));
+let fancyToggle = document.querySelector('#fc');
+if (fancyToggle) fancyToggle.oninput=()=> fancyCards = !!fancyToggle.checked;
